73a189b2a2f2c679c94ad3b37ca86ab673ff7677
huyenmy2004 <132880349+huyenmy2004@users.noreply.github.com>
2023-08-16 21:31:06 +0700

    Cart Done

---
+++ cart/action-insert-order.php
@@ -0,0 +1,12 @@
+<?php
+    require_once 'cart-pdo.php';
+    $cart = new Cart();
+    $listProd = $cart->getData();
+
+    $data = [
+        'orderId' => 'order'.$cart->totalOrder()['total'],
+        'note' => $_POST['note']
+    ];
+    $cart->insertOrder($data, $listProd);
+    header("Location: http://localhost/PharmaDI-Enduser/cart/cart-list.php");
+?>
