f33f68fa07159c8c34fc2fd6c826d2e6d1f23845
toan-vu <132880847+toan-vu@users.noreply.github.com>
2023-08-16 22:15:01 +0700

    update

--- order/order.php
+++ order/order.php
@@ -12,38 +12,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="order.css">
-    <!-- <link rel="stylesheet" href="font_backgroud.css"> -->
     <title>Order</title>
 </head>
 <body>
-    <!-- <div class="menu">
-        <div class="menu-top">
-            <div class="img">
-                <img src="../asset/image/logo.png" alt="logo">
-            </div>
-            <div class="menu-mid">
-                <form action="">
-                    <input type="text" placeholder="Nhập nội dung tìm kiếm..." class="search">
-                    <button>
-                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M9.58332 2.29166C5.55625 2.29166 2.29166 5.55625 2.29166 9.58332C2.29166 13.6104 5.55625 16.875 9.58332 16.875C13.6104 16.875 16.875 13.6104 16.875 9.58332C16.875 5.55625 13.6104 2.29166 9.58332 2.29166ZM1.04166 9.58332C1.04166 4.86589 4.86589 1.04166 9.58332 1.04166C14.3008 1.04166 18.125 4.86589 18.125 9.58332C18.125 11.7171 17.3426 13.6681 16.049 15.1652L18.7753 17.8914C19.0193 18.1355 19.0193 18.5312 18.7753 18.7753C18.5312 19.0193 18.1355 19.0193 17.8914 18.7753L15.1652 16.049C13.6681 17.3426 11.7171 18.125 9.58332 18.125C4.86589 18.125 1.04166 14.3008 1.04166 9.58332Z" fill="#505050"/>
-                        </svg>                
-                    </button>
-                </form>
-            </div>
-            <div class="menu-right">
-                <button>Đăng ký</button>
-                <button>Đăng nhập</button>
-            </div>
-        </div>
-        <div class="menu-bottom">
-            <a href="" class="menu-active">TRANG CHỦ</a>
-            <a href="">SẢN PHẨM</a>
-            <a href="">THƯƠNG HIỆU</a>
-            <a href="">TIN TỨC</a>
-        </div>
-    </div> -->
 
+<div class="content">
     <div class="order-list">
         <h1 class="order-list-title">Danh sách đơn hàng</h1>
         <?php 
@@ -111,71 +84,6 @@
                     <?php endforeach; ?>
                 </div>
             </div> 
-        
-        <!-- <div class="order-lists">
-            <div class="order-list-box active">
-                <div class="order-list-detail">
-                    <div class="order-list-item">
-                        <div class="order-list-item-des">
-                            <img class="order-list-item-img" src="image/Rectangle 275.png" alt="">
-                            <ul class="order-list-item-inf">
-                                <li><button class="order-list-item-status">Đã xác nhận</button></li>
-                                <li><span class="order-list-item-title">Methycobal Mecobalamin 500mcg/Ml Eisai (H/10o/1ml)</span></li>
-                                <li><p class="order-list-item-price">378.900đ</p></li>
-                            </ul>
-                        </div>
-                        <p class="order-list-item-detal"><a href="order-detail.php">Xem thêm ></a></p>
-                        <p class="order-list-item-num">2 sản phẩm</p>
-                        <p class="order-list-item-total">Thành tiền: 3.000.000đ</p>
-                    </div>
-                </div>
-                <div class="order-list-detail">
-                    <div class="order-list-item">
-                        <div class="order-list-item-des">
-                            <img class="order-list-item-img" src="image/Rectangle 275.png" alt="">
-                            <ul class="order-list-item-inf">
-                                <li><button class="order-list-item-status">Chờ xác nhận</button></li>
-                                <li><span class="order-list-item-title">Methycobal Mecobalamin 500mcg/Ml Eisai (H/10o/1ml)</span></li>
-                                <li><p class="order-list-item-price">378.900đ</p></li>
-                            </ul>
-                        </div>
-                        <p class="order-list-item-detal"><a href="order-detail.html">Xem thêm ></a></p>
-                        <p class="order-list-item-num">2 sản phẩm</p>
-                        <p class="order-list-item-total">Thành tiền: 3.000.000đ</p>
-                    </div>
-                </div>
-                <div class="order-list-detail">
-                    <div class="order-list-item">
-                        <div class="order-list-item-des">
-                            <img class="order-list-item-img" src="image/Rectangle 275.png" alt="">
-                            <ul class="order-list-item-inf">
-                                <li><button class="order-list-item-status">Chưa xác nhận</button></li>
-                                <li><span class="order-list-item-title">Methycobal Mecobalamin 500mcg/Ml Eisai (H/10o/1ml)</span></li>
-                                <li><p class="order-list-item-price">378.900đ</p></li>
-                            </ul>
-                        </div>
-                        <p class="order-list-item-detal"><a href="order-detail.html">Xem thêm ></a></p>
-                        <p class="order-list-item-num">2 sản phẩm</p>
-                        <p class="order-list-item-total">Thành tiền: 3.000.000đ</p>
-                    </div>
-                </div>
-                <div class="order-list-detail">
-                    <div class="order-list-item">
-                        <div class="order-list-item-des">
-                            <img class="order-list-item-img" src="image/Rectangle 275.png" alt="">
-                            <ul class="order-list-item-inf">
-                                <li><button class="order-list-item-status">Chờ xác nhận</button></li>
-                                <li><span class="order-list-item-title">Methycobal Mecobalamin 500mcg/Ml Eisai (H/10o/1ml)</span></li>
-                                <li><p class="order-list-item-price">378.900đ</p></li>
-                            </ul>
-                        </div>
-                        <p class="order-list-item-detal"><a href="order-detail.html">Xem thêm ></a></p>
-                        <p class="order-list-item-num">2 sản phẩm</p>
-                        <p class="order-list-item-total">Thành tiền: 3.000.000đ</p>
-                    </div>
-                </div>
-            </div>
-        </div> -->
 
         <!-- search wait -->
 
@@ -296,34 +204,8 @@
                 <?php } endforeach; ?>
             </div>
         </div> 
-        
     </div>
-    <!-- <div class="footer">
-        <div class="footer-top">
-            <div class="footer-left">
-                <div class="footer-left-top">
-                    <span style="font-weight: 600; padding-bottom: 20px; font-size: 24px;">PharmaDI</span>
-                    <span>Nền tảng B2B phân phối Thực Phẩm Chức Năng Chính Hãng</span>
-                </div>
-                <div class="footer-left-bottom">
-                    <span style="font-weight: 600; padding-bottom: 20px; font-size: 18px;">CÔNG TY CỔ PHẦN PHARMADI</span>
-                    <span>Trụ sở: 36 Mạc Đỉnh Chi, Phường Đa Kao, Quận 1, Thành phố Hồ Chí Minh, Việt Nam</span>
-                    <span>Giấy phép ĐKKD số 0317699732 do Sở Kế hoạch và Đầu tư Thành phố Hà Nội Cấp ngày 23/02/2023</span>
-                    <span>Hotline: (028) 888 59 468</span>
-                    <span>Email: info@pharmadi.vn</span>
-                </div>
-            </div>
-            <div class="footer-right">
-                <span>CHÍNH SÁCH</span>
-                <a href="">Chính sách vận chuyển</a>
-                <a href="">Chính sách bảo mật</a>
-                <a href="">Chính sách đổi trả, kiểm hàng</a>
-                <a href="">Chính sách thanh toán</a>
-                <a href="">Điều khoản sử dụng</a>
-            </div>
-        </div>
-        <div class="footer-bottom">© Copyright PharmaDi. All rights reserved.</div>
-    </div> -->
+</div>
     <script src="app.js"></script>
     <?php require_once '../components/footer.php'; ?>
 </body>
